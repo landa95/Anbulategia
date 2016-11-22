6b34bc0715dbd8835356307463783993670dab3f
eruizdeloizaga002 <enekoruizdeloizaga@gmail.com>
2016-11-22 18:38:53 +0100

    Update Gaisoa.java

--- src/java/Gaisoa.java
+++ src/java/Gaisoa.java
@@ -1,17 +1,120 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author Oier
- */
-class Gaisoa {
-
-    Gaisoa(String pIzena, String pAbizena1, String pAbizena2, int pAdina, int pTelf, String pHelb, int pGSZ) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+private String izena;
+    private String abizena1;
+    private String abizena2;
+    private int adina;
+    private int telefonoa;
+    private String bizilekua;
+    private int GSZ;
+    private boolean pentsioduna = false;
+    //private bajaZerrenda bajaZerrenda;
+    //private Historiala historiala
+    //private Txostena txostena;
+    //private ArrayList<Errezeta> errezetak = new ArrayList<Errezeta>();
+    //private Sendagilea sendagilea;
+    
+   
+    public Gaisoa(String izena, String abizena1, String abizena2, int adina, int telefonoa, String bizilekua, int GSZ){
+ 
+        this.izena = izena;
+        this.abizena1 = abizena1;
+        this.abizena2 = abizena2;
+        this.adina = adina;
+            if(adina > 60){
+                pentsioduna = true;
+            }
+        this.telefonoa = telefonoa;
+        this.bizilekua = bizilekua;
+        this.GSZ = GSZ;
+       
     }
     
+    //GETTER
+    
+    public String getIzena(){
+        return izena;
+    }
+   
+    public String getAbizena1(){
+        return abizena1;
+    }
+    
+     public String getAbizena2(){
+        return abizena2;
+    }
+    
+    public int getAdina(){
+        return adina;
+    }
+    
+    public int getTelefonoa(){
+        return telefonoa;
+    }
+    
+    public String getBizilekua(){
+        return bizilekua;
+    }
+    
+    public int getTxartela(){
+        return GSZ;
+    }
+    
+    public ArrayList getDatuPertsonalak(){
+    
+        ArrayList lista = new ArrayList();
+
+        lista.add(izena);
+        lista.add(abizena1);
+        lista.add(abizena2);
+        lista.add(adina);
+        lista.add(telefonoa);
+        lista.add(bizilekua);
+        lista.add(GSZ);
+        
+        return lista;
+    }
+    
+    public boolean getPentsioduna(){
+        return pentsioduna;
+    }
+    
+
+    //SETTER
+    
+    public void setIzena(String izena){
+        this.izena = izena;
+    }
+   
+    public void setAbizena1(String abizena){
+        this.abizena1 = abizena;
+    }
+    
+    public void setAbizena2(String abizena){
+        this.abizena2 = abizena;
+    }
+    
+    public void setAdina(int adina){
+        this.adina = adina;
+    }
+    
+    public void setTelefonoa(int telefonoa){
+        this.telefonoa = telefonoa;
+    }
+    
+    public void setBizilekua(String bizilekua){
+        this.bizilekua = bizilekua;
+    }
+    
+    public void setTxartela(int txartelzenb){
+        this.GSZ = txartelzenb;
+    }
+
+
+    //METODOAK
+    
+    public void kontsultaEskatu(){
+    
+    }
+    
+    
+
 }
